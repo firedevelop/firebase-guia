9d0e02cdd7fdf20b09b107e583a3ee1d713a5db4
firedevelop <firedevelop.com@gmail.com>
2025-12-25 15:06:52 +0100

    feat(actividad-02): add readme

--- 02-configuracion-inicial/firebase-config.js
+++ 02-configuracion-inicial/firebase-config.js
@@ -1,8 +1,8 @@
 // CAMBIO IMPORTANTE: Usamos la URL completa para que funcione sin instalar nada extra
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-// Tu configuración real (la que me acabas de pasar)
 const firebaseConfig = {
+  
   apiKey: "AIzaSyBIKv5CHHzVHTjVp_se7B5sRzahOdcoZPQ",
   authDomain: "panaderia-backend-test.firebaseapp.com",
   projectId: "panaderia-backend-test",
